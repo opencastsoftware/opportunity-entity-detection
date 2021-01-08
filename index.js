f8c0291fa5556718a00e8331384e16d2204a1e70
Rob Bulmer <r95@hotmail.co.uk>
2021-01-08 11:37:05 +0000

    reinstate essential skills

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const osmosis = require("osmosis");
 
+const AWS = require("aws-sdk");
+const fetch = require('node-fetch');
+
+AWS.config.update({ region: "eu-west-2" });
+// const comprehend = new AWS.Comprehend({ apiVersion: '2017-11-27' });
+
 // const graphUtils = require('./helpers/graphUtils');
 // const essentialSkillsModule  = require('./helpers/getEssentialSkills');
-const niceToHaveSkillsModule  = require('./helpers/getNiceToHaveSkills');
-const entitiesModule = require('./helpers/getEntities');
+// const niceToHaveSkillsModule  = require('./helpers/getNiceToHaveSkills');
+// const entitiesModule = require('./helpers/getEntities');
 
 const baseUrl = 'https://www.digitalmarketplace.service.gov.uk';
 
 
-async function getEssentialSkills(url){
+function getEssentialSkills(url) {
     console.log("getting skills for: " + url);
     let text;
 
@@ -26,33 +32,32 @@ async function getEssentialSkills(url){
             .error((err) => console.log(err))
             .done(() => resolve(text));
     });
-};
+}
 
 const handler = async (event) => {
     try{
         await Promise.all(event.Records.map(async (record) => {
             const url = baseUrl + record.messageAttributes.Link.stringValue;
-            const {
-                Organisation: {stringValue: organisation}, 
-                Location: {stringValue:location},
-                Title: {stringValue:title},
-                Type: {stringValue: type},
-                ClosingDate: {stringValue:closingDate},
-                ID: {stringValue:id}
-            } = record.messageAttributes;
+            // const {
+            //     Organisation: {stringValue: organisation}, 
+            //     Location: {stringValue:location},
+            //     Title: {stringValue:title},
+            //     Type: {stringValue: type},
+            //     ClosingDate: {stringValue:closingDate},
+            //     ID: {stringValue:id}
+            // } = record.messageAttributes;
 
             //console.log('creating organisation, location and opportunity vertex');
             // await graphUtils.createOrganisation(organisation);
             // await graphUtils.createLocation(location);
             // await graphUtils.createOpportunity({id, title, date: closingDate, type});
 
-            console.log('calling get essential skills for url', url);
             const eSkills = await getEssentialSkills(url);
             console.log("eSkills:", eSkills);
 
-            console.log('calling get entities for eskills:', eSkills.text);
-            const { Entities: eSkillEntities } = await entitiesModule.getEntities(eSkills.text);
-            console.log("Essential Entities", eSkillEntities);
+            // console.log('calling get entities for eskills:', eSkills.text);
+            // const { Entities: eSkillEntities } = await entitiesModule.getEntities(eSkills.text);
+            // console.log("Essential Entities", eSkillEntities);
 
             // only interested in TITLE entities
             // const keyEssentialEntities = eSkillEntities.filter(entity => entity.Type === 'TITLE');
@@ -94,31 +99,6 @@ const handler = async (event) => {
       };
 
 }
-
-// const testEvent = {
-//     "Records": [
-//         {
-//             "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
-//             "receiptHandle": "MessageReceiptHandle",
-//             "body": "Hello from SQS!",
-//             "attributes": {
-//                 "ApproximateReceiveCount": "1",
-//                 "SentTimestamp": "1523232000000",
-//                 "SenderId": "123456789012",
-//                 "ApproximateFirstReceiveTimestamp": "1523232000001"
-//             },
-//             "messageAttributes": {
-//                 "Link": "https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/13237"
-//             },
-//             "md5OfBody": "{{{md5_of_body}}}",
-//             "eventSource": "aws:sqs",
-//             "eventSourceARN": "arn:aws:sqs:eu-west-2:123456789012:MyQueue",
-//             "awsRegion": "eu-west-2"
-//         }
-//     ]
-// }
-
-// handler(testEvent);
 
 module.exports = {
     handler
