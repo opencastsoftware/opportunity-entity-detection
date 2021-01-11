b7e8746669b2b91fc9e04fcd2cfd76e01039caf9
Fiona Hobbs <fionamayhobbs@gmail.com>
2021-01-11 14:37:26 +0000

    osmosis to capture opportunity dates

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 
 const graphUtils = require('./helpers/graphUtils');
-const essentialSkillsModule  = require('./helpers/getEssentialSkills');
-const niceToHaveSkillsModule  = require('./helpers/getNiceToHaveSkills');
+const essentialSkillsModule = require('./helpers/getEssentialSkills');
+const niceToHaveSkillsModule = require('./helpers/getNiceToHaveSkills');
 const entitiesModule = require('./helpers/getEntities');
 
 async function handler(event) {
     const baseUrl = 'https://www.digitalmarketplace.service.gov.uk';
 
-    try{
+    try {
         await Promise.all(event.Records.map(async (record) => {
             const url = baseUrl + record.messageAttributes.Link.stringValue;
             const {
-                Organisation: {stringValue: organisation}, 
-                Location: {stringValue:location},
-                Title: {stringValue:title},
-                Type: {stringValue: type},
-                ClosingDate: {stringValue:closingDate},
-                ID: {stringValue:id}
+                Organisation: { stringValue: organisation },
+                Location: { stringValue: location },
+                Title: { stringValue: title },
+                Type: { stringValue: type },
+                ID: { stringValue: id }
             } = record.messageAttributes;
 
             console.log('creating organisation, location and opportunity vertex');
             await graphUtils.createOrganisation(organisation);
             await graphUtils.createLocation(location);
-            await graphUtils.createOpportunity({id, title, date: closingDate, type});
+            await graphUtils.createOpportunity({ id, title, type });
 
             const eSkills = await essentialSkillsModule.getEssentialSkills(url);
             const { Entities: eSkillEntities } = await entitiesModule.getEntities(eSkills.text);
-         
+
 
             // only interested in TITLE entities
             const keyEssentialEntities = eSkillEntities.filter(entity => entity.Type === 'TITLE');
-            if(keyEssentialEntities.length){
+            if (keyEssentialEntities.length) {
                 console.log('Adding key essential entities', keyEssentialEntities);
-                await Promise.all(keyEssentialEntities.map(async ({Text:entityName})=>{
+                await Promise.all(keyEssentialEntities.map(async ({ Text: entityName }) => {
                     // Add the entity
                     await graphUtils.createEntity(entityName);
                     // Add the "ESSENTIAL" edge to the vertex
@@ -42,12 +41,12 @@ async function handler(event) {
 
             const niceSkills = await niceToHaveSkillsModule.getNiceToHaveSkills(url);
             const { Entities: niceToHaveSkillEntities } = await entitiesModule.getEntities(niceSkills.text);
-        
+
             // only interested in TITLE entities
             const keyNiceToHaveEntities = niceToHaveSkillEntities.filter(entity => entity.Type === 'TITLE');
-            if(keyNiceToHaveEntities.length){
+            if (keyNiceToHaveEntities.length) {
                 console.log('Adding key NiceToHave entities', keyNiceToHaveEntities);
-                await Promise.all(keyNiceToHaveEntities.map(async ({Text:entityName})=>{
+                await Promise.all(keyNiceToHaveEntities.map(async ({ Text: entityName }) => {
                     await graphUtils.createEntity(entityName);
                 }));
             }
@@ -64,13 +63,13 @@ async function handler(event) {
         return {
             statusCode: 201,
             body: JSON.stringify("Proccessed the new opportunities!"),
-          };
-    }catch(e){
+        };
+    } catch (e) {
         console.log(e)
         return {
             statusCode: 500,
             body: JSON.stringify("Something went wrong!"),
-          };
+        };
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -8,21 +8,22 @@ const essentialSkillsModule = require('./helpers/getEssentialSkills');
 const niceSkillsModule = require('./helpers/getNiceToHaveSkills');
 const entityModule = require('./helpers/getEntities');
 const entityDetection = require("./index")
+const opportunityDates = require('./helpers/getOpportunityDates');
 
 const essentialSkillSpy = jest.spyOn(essentialSkillsModule, 'getEssentialSkills');
 const niceSkillSpy = jest.spyOn(niceSkillsModule, 'getNiceToHaveSkills');
 
-jest.mock('./helpers/getEntities', ()=>{
-    return{
-        getEntities: jest.fn().mockResolvedValue({Entities:[{Text:'C#', Type:'TITLE'}, {Text:'NodeJS', Type:'TITLE'}]}),
+jest.mock('./helpers/getEntities', () => {
+    return {
+        getEntities: jest.fn().mockResolvedValue({ Entities: [{ Text: 'C#', Type: 'TITLE' }, { Text: 'NodeJS', Type: 'TITLE' }] }),
     }
 });
 
-jest.mock('./helpers/graphUtils', ()=>{
+jest.mock('./helpers/graphUtils', () => {
     return {
         createLocation: jest.fn().mockResolvedValue(true),
-        createOpportunity:jest.fn().mockResolvedValue(true),
-        createOrganisation:jest.fn().mockResolvedValue(true),
+        createOpportunity: jest.fn().mockResolvedValue(true),
+        createOrganisation: jest.fn().mockResolvedValue(true),
         createEntity: jest.fn().mockResolvedValue(true),
         createEssentialEdge: jest.fn().mockResolvedValue(true),
 
@@ -32,9 +33,9 @@ jest.mock('./helpers/graphUtils', ()=>{
     }
 })
 
-describe('handler', ()=>{
+describe('handler', () => {
     let event;
-    beforeEach(()=>{
+    beforeEach(() => {
         graphModule.createLocation.mockClear();
         graphModule.createOpportunity.mockClear();
         graphModule.createOrganisation.mockClear();
@@ -46,27 +47,24 @@ describe('handler', ()=>{
             Records: [
                 {
                     body: 'User Centred Design Partner',
-                    messageAttributes:{
-                        Link:{
+                    messageAttributes: {
+                        Link: {
                             stringValue: '/digital-outcomes-and-specialists/opportunities/13237'
                         },
                         Organisation: {
                             stringValue: 'test org'
-                        }, 
+                        },
                         Location: {
-                            stringValue:'test location'
+                            stringValue: 'test location'
                         },
                         Title: {
-                            stringValue:'test title'
+                            stringValue: 'test title'
                         },
                         Type: {
                             stringValue: 'test type'
                         },
-                        ClosingDate: {
-                            stringValue:'test date'
-                        },
                         ID: {
-                            stringValue:'test id'
+                            stringValue: 'test id'
                         }
                     }
                 },
@@ -74,44 +72,42 @@ describe('handler', ()=>{
         };
 
         const fixture = fs
-        .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
-        .toString();
-
-
+            .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
+            .toString();
         nock("https://www.digitalmarketplace.service.gov.uk")
             .get("/digital-outcomes-and-specialists/opportunities/13237")
             .reply(200, fixture);
     });
 
-    it('should call getEssentialSkills with the correct url for each message', async ()=>{ 
+    it('should call getEssentialSkills with the correct url for each message', async () => {
         const result = await entityDetection.handler(event);
         expect(essentialSkillSpy).toHaveBeenCalledWith('https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/13237');
-        expect(result).toEqual({"body": "\"Proccessed the new opportunities!\"", "statusCode": 201});
+        expect(result).toEqual({ "body": "\"Proccessed the new opportunities!\"", "statusCode": 201 });
     })
 
-    it('should call getNiceToHaveSkills with the correct url for each message', async ()=>{ 
+    it('should call getNiceToHaveSkills with the correct url for each message', async () => {
         const result = await entityDetection.handler(event);
         expect(niceSkillSpy).toHaveBeenCalledWith('https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/13237');
-        expect(result).toEqual({"body": "\"Proccessed the new opportunities!\"", "statusCode": 201});
+        expect(result).toEqual({ "body": "\"Proccessed the new opportunities!\"", "statusCode": 201 });
     })
 
 
-    it('should call getEntities with the correct url for each message', async ()=>{ 
+    it('should call getEntities with the correct url for each message', async () => {
 
         const result = await entityDetection.handler(event);
         expect(entityModule.getEntities).toHaveBeenCalledWith('Have experience of developing and establishing enterprise analytics and data innovation strategies, along with associated implementation plans;\n' +
-        '          Have experience providing subject matter expertise across a wide range of internally and externally facing projects to conceptualize, design and deliver data analytics and insight projects;\n' +
-        '          Demonstrable experience supporting agile delivery team’s application of data science, statistical analysis and geospatial mapping whilst seamlessly integrating into client teams;\n' +
-        '          Experience of eliciting analytical requirements from a range of stakeholders, understand user needs and communicating technical/data principles, tools, techniques and requirements between non-technical business audiences and technical specialists;\n' +
-        '          Practical experience identifying business problems and datasets suitable for data science and the development of new products/services, as well as prioritising, planning and executing Research & Development activities;\n' +
-        '          Experience designing and deliver engaging data stories using effective data visualisation supporting analysis communication. Demonstrate a range of techniques, including dashboarding, in open source and enterprise reporting tools (e.g. PowerBI);\n' +
-        '          Have experience of delivering performance analysis for digital services including use of A/B testing, Google Analytics and querying relational databases to gather data for analysis;\n' +
-        '          Evidence of ability to discover and assess data for development of new products/services, including objective determination of data quality/‘fit for purpose’, understanding caveats and limitations, relationships and linkages;\n' +
-        '          Delivery of geospatial analysis, visualisation and services to support digital transformation programmes (using tools like ArcGIS Enterprise / Pro / FME / QGIS);\n' +
-        '          Demonstrable practical experience in delivering high value end-to-end data science projects in production environments, using modern architectures, cloud compute platforms and both open source and enterprise tools;\n' +
-        '          Significant practical experience in leading edge data science and machine learning techniques applied at scale, particularly in the fields of NLP, geospatial, computer vision and knowledge graphs/mining;\n' +
-        '          Practical experience of operating and maintaining end-to-end platforms for robust, low latency production data science, with associated monitoring, assurance and governance.');
-        expect(result).toEqual({"body": "\"Proccessed the new opportunities!\"", "statusCode": 201});
+            '          Have experience providing subject matter expertise across a wide range of internally and externally facing projects to conceptualize, design and deliver data analytics and insight projects;\n' +
+            '          Demonstrable experience supporting agile delivery team’s application of data science, statistical analysis and geospatial mapping whilst seamlessly integrating into client teams;\n' +
+            '          Experience of eliciting analytical requirements from a range of stakeholders, understand user needs and communicating technical/data principles, tools, techniques and requirements between non-technical business audiences and technical specialists;\n' +
+            '          Practical experience identifying business problems and datasets suitable for data science and the development of new products/services, as well as prioritising, planning and executing Research & Development activities;\n' +
+            '          Experience designing and deliver engaging data stories using effective data visualisation supporting analysis communication. Demonstrate a range of techniques, including dashboarding, in open source and enterprise reporting tools (e.g. PowerBI);\n' +
+            '          Have experience of delivering performance analysis for digital services including use of A/B testing, Google Analytics and querying relational databases to gather data for analysis;\n' +
+            '          Evidence of ability to discover and assess data for development of new products/services, including objective determination of data quality/‘fit for purpose’, understanding caveats and limitations, relationships and linkages;\n' +
+            '          Delivery of geospatial analysis, visualisation and services to support digital transformation programmes (using tools like ArcGIS Enterprise / Pro / FME / QGIS);\n' +
+            '          Demonstrable practical experience in delivering high value end-to-end data science projects in production environments, using modern architectures, cloud compute platforms and both open source and enterprise tools;\n' +
+            '          Significant practical experience in leading edge data science and machine learning techniques applied at scale, particularly in the fields of NLP, geospatial, computer vision and knowledge graphs/mining;\n' +
+            '          Practical experience of operating and maintaining end-to-end platforms for robust, low latency production data science, with associated monitoring, assurance and governance.');
+        expect(result).toEqual({ "body": "\"Proccessed the new opportunities!\"", "statusCode": 201 });
     })
 
     it('should call graph utils create location', async () => {
@@ -126,7 +122,7 @@ describe('handler', ()=>{
 
     it('should call graph utils create opportunity', async () => {
         await entityDetection.handler(event);
-        expect(graphModule.createOpportunity).toHaveBeenCalledWith({"date": "test date", "id": "test id", "title": "test title", "type": "test type"});
+        expect(graphModule.createOpportunity).toHaveBeenCalledWith({ "id": "test id", "title": "test title", "type": "test type" });
     })
 
     it('should call graph utils create entity for each entity found', async () => {
@@ -140,13 +136,13 @@ describe('handler', ()=>{
         expect(graphModule.createEssentialEdge).toHaveBeenCalledWith('C#', 'test id');
         expect(graphModule.createEssentialEdge).toHaveBeenCalledWith('NodeJS', 'test id');
     })
-   
+
 });
 
 describe("get opportunity essential skill", () => {
     const fixture = fs
-    .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
-    .toString();
+        .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
+        .toString();
 
     const url = "https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/13237"
 
@@ -173,8 +169,8 @@ describe("get opportunity essential skill", () => {
 
 describe("get opportunity nice-to-have skill", () => {
     const fixture = fs
-    .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
-    .toString();
+        .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
+        .toString();
 
     const url = "https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/13237"
 
@@ -184,17 +180,46 @@ describe("get opportunity nice-to-have skill", () => {
         .reply(200, fixture);
 
     it("should return the nice-to-have skills text", async () => {
-    const skillsText = await niceSkillsModule.getNiceToHaveSkills(url);
-    expect(skillsText.text).toEqual('Experience of evaluating the impact of service delivery, including approaches set out in the Magenta Book (https://www.gov.uk/government/publications/the-magenta-book);\n' +
-        '          Experience creating using and interpreting data specifications and standards;\n' +
-        '          Experience identifying interactions and interdependencies across data sets to enable integration of data to support new services;\n' +
-        '          Ability to objectively demonstrate how previous collaborative experiences have measurably grown organisational capability through knowledge exchange;\n' +
-        '          Demonstrable experience of promoting a collaborative culture and data curiosity organisations, particularly when working in partnership with customers and as part of a multi-supplier teams;\n' +
-        '          Practical experience of establishing and executing processes to take analytics from exploratory/development environments into production;\n' +
-        '          Practical experience developing and maintaining reproducible data science pipelines using distributed compute over multi-cloud environments;\n' +
-        '          Experience deploying and operating enterprise-level, analytics platforms on premises and in-cloud;\n' +
-        '          Experience of writing and presenting engaging, clear and effective technical documentation, reports, papers, scientific manuscripts and presentations (and coaching others to do the same);\n' +
-        '          Experience building services to Government ethics guidance, data security best practice and using privacy preserving methodologies in analytics;\n' +
-        '          Practical analytics/data engineering with (or Accredited training in):   ArcGIS/QGIS,   Python (scientific/geospatial/ML stacks) and/or Scala,   Distributed compute (Dask/Spark),   Deep Learning frameworks (Tensorflow, PyTorch etc.),   Graph databases,   Linked open data (RDF, SPARQL).');
+        const skillsText = await niceSkillsModule.getNiceToHaveSkills(url);
+        expect(skillsText.text).toEqual('Experience of evaluating the impact of service delivery, including approaches set out in the Magenta Book (https://www.gov.uk/government/publications/the-magenta-book);\n' +
+            '          Experience creating using and interpreting data specifications and standards;\n' +
+            '          Experience identifying interactions and interdependencies across data sets to enable integration of data to support new services;\n' +
+            '          Ability to objectively demonstrate how previous collaborative experiences have measurably grown organisational capability through knowledge exchange;\n' +
+            '          Demonstrable experience of promoting a collaborative culture and data curiosity organisations, particularly when working in partnership with customers and as part of a multi-supplier teams;\n' +
+            '          Practical experience of establishing and executing processes to take analytics from exploratory/development environments into production;\n' +
+            '          Practical experience developing and maintaining reproducible data science pipelines using distributed compute over multi-cloud environments;\n' +
+            '          Experience deploying and operating enterprise-level, analytics platforms on premises and in-cloud;\n' +
+            '          Experience of writing and presenting engaging, clear and effective technical documentation, reports, papers, scientific manuscripts and presentations (and coaching others to do the same);\n' +
+            '          Experience building services to Government ethics guidance, data security best practice and using privacy preserving methodologies in analytics;\n' +
+            '          Practical analytics/data engineering with (or Accredited training in):   ArcGIS/QGIS,   Python (scientific/geospatial/ML stacks) and/or Scala,   Distributed compute (Dask/Spark),   Deep Learning frameworks (Tensorflow, PyTorch etc.),   Graph databases,   Linked open data (RDF, SPARQL).');
     });
+});
+
+describe("get opporunity details", () => {
+    const fixture = fs
+        .readFileSync(join(__dirname, "fixtures/test.html"), "utf-8")
+        .toString();
+
+    const url = "https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/13237"
+
+    nock("https://www.digitalmarketplace.service.gov.uk")
+        .persist()
+        .get("/digital-outcomes-and-specialists/opportunities/13237")
+        .reply(200, fixture);
+
+    it("should set the published date", async () => {
+        const opportunity = await opportunityDates.getDates(url);
+        expect(opportunity.publishedDate).toEqual(1601938800000);
+    });
+
+    it("should set the questions deadline date", async () => {
+        const opportunity = await opportunityDates.getDates(url);
+        expect(opportunity.questionsDeadlineDate).toEqual(1602543600000);
+    });
+
+    it("should set the closing date", async () => {
+        const opportunity = await opportunityDates.getDates(url);
+        expect(opportunity.closingDate).toEqual(1603148400000);
+    });
+
 });
